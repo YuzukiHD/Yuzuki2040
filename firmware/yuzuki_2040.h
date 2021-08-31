e15c8ff4b66acf9d2d649fb0ac79a7cc99dc7211
柚木鉉 <740291272@qq.com>
2021-08-31 14:14:13 +0800

    Update yuzuki_2040.h

--- firmware/yuzuki_2040.h
+++ firmware/yuzuki_2040.h
@@ -4,11 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-// -----------------------------------------------------
-// NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
-//       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
-// -----------------------------------------------------
-
 #ifndef _BOARDS_YUZUKI_2040_H
 #define _BOARDS_YUZUKI_2040_H
 
